7db76066a3cfca2b73fe1c7f0422becf4d577931
Winston <winstonwut@gmail.com>
2015-09-16 08:16:03 -0400

    Fixed Syntax errors, testing works

--- app/src/main/java/com/rut0/sqllistsync/DbHelper.java
+++ app/src/main/java/com/rut0/sqllistsync/DbHelper.java
@@ -9,40 +9,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by Winston on 9/13/2015.
  */
-public class DbHelper extends SQLiteOpenHelper {
+public class DbHelper {
 
     public static final String TABLE_NAME = "messages";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_MESSAGE = "message";
     public SQLiteDatabase database;
+    private Helper _helper;
 
     public static final String DATABASE_NAME = "message.db";
     public static final int DATABASE_VERSION = 1;
 
-    public static final String CREATE_TABLE = "CREATE TABLE" +
+    public static final String CREATE_TABLE = "CREATE TABLE " +
             TABLE_NAME + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             COLUMN_NAME + " TEXT NOT NULL, " +
             COLUMN_MESSAGE + " TEXT);";
 
     public DbHelper(Context ctx) {
-        super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE);
-        database = db;
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
+        _helper = new Helper(ctx);
     }
 
     public Cursor getMessages() {
         Cursor c = null;
+        if (database == null)
+            database = _helper.open();
         c = database.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_NAME, COLUMN_MESSAGE}, null, null, null, null, null);
         return c;
     }
@@ -52,5 +43,26 @@ public class DbHelper extends SQLiteOpenHelper {
         vals.put(COLUMN_NAME, name);
         vals.put(COLUMN_MESSAGE, message);
         return database.insert(TABLE_NAME, null, vals) != -1;
+    }
+
+    private static class Helper extends SQLiteOpenHelper {
+        public Helper(Context ctx) {
+            super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+
+        public SQLiteDatabase open() {
+            return getWritableDatabase();
+        }
     }
 }
